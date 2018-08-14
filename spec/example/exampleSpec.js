845876208d610150f50a535f0da3cefa85e72905
Celio Paiva <celiopaivajr@gmail.com>
2018-08-14 11:47:06 -0300

    Add Browserify to fix Karma Require errors

--- spec/example/exampleSpec.js
+++ spec/example/exampleSpec.js
@@ -2,4 +2,8 @@ describe('Suite  de teste de exemplo com NodeJS', () => {
     it('Deve demonstrar o uso do Jasmine com NodeJS', () => {
         expect(true).toEqual(true)
     });
+
+    it('Deve demonstrar o uso do karma em modo de escuta', () => {
+        expect(1).toEqual(1)
+    });
 });
